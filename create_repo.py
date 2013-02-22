9005d0c4cf1b4843aa72f64dceeabac399be6789
Jon Morehouse <morehousej09@gmail.com>
2013-02-22 11:26:25 -0800

    Added readme.md. Added starter github api project.

--- create_repo.py
+++ create_repo.py
@@ -1,15 +1,50 @@
 #!/usr/bin/python
-
-import json
+import json, urllib2
 
 try:
 
 	with open("config.json", "r") as rawJson:
-		config = json.loads(rawJson)
+
+		# 
+		config = json.loads(rawJson.read())
 
 except Exception, e:
 
 	print e
-	raise e
+	exit()
 
-print config
+
+def encodeUserData(user, password):
+    return "Basic " + (user + ":" + password).encode("base64").rstrip()
+
+#
+url = 'https://api.github.com/users/%s/repos' % (config['username'])
+req = urllib2.Request(url)
+
+
+connection = 
+headers = {
+	
+	"Accept" : "application/json",
+	"Content": "application/x-www-form-urlencoded",
+	"Authorization" : encodeUserData(config['username'], config['password']),
+
+}
+
+data = {
+	
+
+	
+
+
+}
+
+h.request('POST', url, data, headers)
+
+# make the request
+response = request.getresponse()
+
+print res.read()
+
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+Python Scripts
+=
+
+Overview
+=
+
+This is a general repository to catch and version assortments of my python scripts. These are usually quick programs that accomplish a single task. Also a place to play around with new technologies and tools.
