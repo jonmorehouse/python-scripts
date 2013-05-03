27fe217966e555a6619fcbe2dd044b89468f2974
Jon Morehouse <morehousej09@gmail.com>
2013-05-03 15:12:33 -0700

    Create a basic fibonacci printer and play around with some python features

---
+++ fibonacci.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# create a fibonacci lambda
+fibonacci = lambda previous, current: previous + current if previous + current > 0 else 1
+
+# prime number printer etc
+# prime = lambda 
+
+
+# controller is useful for printing out valid information for the actual callback / lambda etc
+def controller(previous, current, counter, callback):
+
+	# grab the next element
+	next = fibonacci(previous, current)
+
+	# call the passed in callback etc for this element!
+	callback(next)
+
+	# now we want to recursively run this function!
+	if counter > 0:
+
+		return controller(current, next, counter-1, callback)
+
+
+# create a basic print function
+def printer(_input):
+
+	# this is customizable!
+	print "%d" % (_input)
+
+
+# now initialize our fibonacci printer etc to do this properly!
+controller(0,0,20,printer)
